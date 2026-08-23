419787c0b5a5d33968546a184e334d01078c1eb3
Mayuri <mayuripaliwal8@gmail.com>
2026-08-23 19:13:02 +0530

    test: fix frontend test

--- frontend/src/Home.test.jsx
+++ frontend/src/Home.test.jsx
@@ -3,13 +3,13 @@ import { render,screen } from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
-//tests whether the text "URL Shortener" appears on the Home Page
-/*test("Home page renders",()=>{
+//tests whether the text "Features" appears on the Home Page
+test("Home page renders",()=>{
     render(
         <BrowserRouter>
             <App/>
         </BrowserRouter>
     );
-    expect(screen.getByText("URL Shortener")).toBeInTheDocument();
-});*/
+    expect(screen.getByText("Features")).toBeInTheDocument();
+});
 
