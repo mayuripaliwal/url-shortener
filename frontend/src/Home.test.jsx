2c49ea37f7b9bb7c6f97102efea5ae3aae93cee8
Mayuri <mayuripaliwal8@gmail.com>
2026-08-25 13:46:35 +0530

    test: update frontend test to check for auth status

--- frontend/src/Home.test.jsx
+++ frontend/src/Home.test.jsx
@@ -3,14 +3,14 @@ import { render,screen } from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
-//tests whether the text "Login" appears on the Home Page
-//Login should show when user is not logged in
-test("Home page renders",()=>{
+//tests whether the text "Checking login..." appears on the Home Page
+//"Checking login..." should show when user is not logged in
+test("Auth check page renders",()=>{
     render(
         <BrowserRouter>
             <App/>
         </BrowserRouter>
     );
-    expect(screen.getByText("Login")).toBeInTheDocument();
+    expect(screen.getByText("Checking login...")).toBeInTheDocument();
 });
 
