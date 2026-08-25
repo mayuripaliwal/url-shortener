1104065f48d9398714071d89e2796c72c0804dd8
Mayuri <mayuripaliwal8@gmail.com>
2026-08-25 13:40:38 +0530

    fix: update frontend test

--- frontend/src/Home.test.jsx
+++ frontend/src/Home.test.jsx
@@ -3,13 +3,14 @@ import { render,screen } from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
-//tests whether the text "Features" appears on the Home Page
+//tests whether the text "Login" appears on the Home Page
+//Login should show when user is not logged in
 test("Home page renders",()=>{
     render(
         <BrowserRouter>
             <App/>
         </BrowserRouter>
     );
-    expect(screen.getByText("Features")).toBeInTheDocument();
+    expect(screen.getByText("Login")).toBeInTheDocument();
 });
 
