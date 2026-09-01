01a80badbde73cc0607fed6c14d7b09ee3d67944
Mayuri <mayuripaliwal8@gmail.com>
2026-09-01 10:59:37 +0530

    feat: add login link on register page

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import {NavLink} from 'react-router-dom';
-
+import Login from './Login';
+import {Link} from "react-router-dom";
 function Register(){
     const [userEmail,setUserEmail]=useState("")
     const [userPassword,setUserPassword]=useState("")
@@ -69,84 +70,129 @@ function Register(){
     }
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="relative isolate bg-white">
+            <div className="relative isolate bg-white">
+                {/*background*/}
 
-            <div
+                <div
                 aria-hidden="true"
                 className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl"
-            >
+                >
 
-            <div
-                className="relative left-1/2 aspect-1155/678 w-[72rem] -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-                style={{
-                    clipPath:
-                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                }}
-            />
+                    <div
+                    className="relative left-1/2 aspect-1155/678 w-[72rem] -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                    style={{
+                        clipPath:
+                        'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    >
+                    </div>
 
-        </div>
-        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900" >
-            Create an account
-        </h2>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="mt-2">
-        <input
-        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-        type="text"
-        placeholder="Enter your username"
-        value={userName}
-        onChange={(e)=>setUserName(e.target.value)}
-        required/>
-        </div>
-        <br></br>
-        <div>
-        <input
-        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-        type="email"
-        placeholder="Enter your email"
-        value={userEmail}
-        onChange={(e)=>setUserEmail(e.target.value)}
-        required/>
-        </div>
-        <br></br>
-        <div>
-        <input
-        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-        type="password"
-        placeholder="Enter your password"
-        value={userPassword}
-        onChange={(e)=>setUserPassword(e.target.value)}
-        required/>
-        </div>
-        <br></br>
+                </div>
 
-
-        <div>
-        {errorMessage && (
-            <p className="mt-4 text-center text-sm text-red-600">{errorMessage}</p>
-        )}
-        </div>
-
-        {loading &&(
-            <p className="mt-4 text-center text-sm text-gray-600">Just a moment...</p>
-        )}
-
-        <br></br>
-
-        <div>
-        <button
-        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={handleSignUp}
-        >Sign Up</button>
-        </div>
-
+                {/*Sign up form*/}
+                {!isRegistered &&
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900" >
+                    Create an account
+                    </h2>
+                }
         
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    {!isRegistered &&
+                    <div className="mt-2">
+                        <label 
+                        className="mb-2 text-left block text-sm/6 font-medium text-gray-900">
+                            Username
+                        </label>
+                        <input
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        type="text"
+                        placeholder="Enter your username"
+                        value={userName}
+                        onChange={(e)=>setUserName(e.target.value)}
+                        required/>
+                    </div>
+                    }
+            
+                    <br></br>
+                    {!isRegistered &&
+                        <div>
+                        <label 
+                        className="mb-2 text-left block text-sm/6 font-medium text-gray-900">
+                            Email address
+                        </label>
+                        <input
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        type="email"
+                        placeholder="Enter your email"
+                        value={userEmail}
+                        onChange={(e)=>setUserEmail(e.target.value)}
+                        required/>
+                        <br></br>
+                        </div>
+                    }
+            
 
-        {isRegistered && (
-            <p className="mt-4 text-center text-sm text-gray-600">Registered Successfully</p>
-        )}
-        </div>
-        </div>
+                    {!isRegistered &&
+                        <div>
+                            
+                        <label 
+                        className="mb-2 text-left block text-sm/6 font-medium text-gray-900">
+                            Password
+                        </label>
+                        <input
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={userPassword}
+                        onChange={(e)=>setUserPassword(e.target.value)}
+                        required/>
+                        <br></br>
+                        </div>
+                    }
+            
+                    
+            
+                    <div>
+                    {errorMessage && (
+                        <p className="mt-4 text-center text-sm text-red-600">{errorMessage}</p>
+                    )}
+                    </div>
+
+                    {loading &&(
+                        <p className="mt-4 text-center text-sm text-gray-600">Just a moment...</p>
+                    )}
+
+            
+
+                    {!isRegistered &&
+                        <div>
+                            <button
+                            className="mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            onClick={handleSignUp}
+                            >Sign Up</button>
+                        </div>
+                    }
+            
+                    {!isRegistered &&
+                        <p className="mt-10 text-center text-sm/6 text-gray-500">
+                            Already have an account?{' '}
+                            <Link to="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            Login
+                            </Link>
+                        </p>
+                    }
+                    {isRegistered && (
+                        <div className="mt-4 text-center text-m text-gray-900">
+                            Your account has been created successfully.{' '}
+                            <Link
+                                to="/login"
+                                className="mt-6 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >Login
+                            </Link>
+                        </div>
+                    )}
+                </div>
+            </div>
 
         </div>
     )
